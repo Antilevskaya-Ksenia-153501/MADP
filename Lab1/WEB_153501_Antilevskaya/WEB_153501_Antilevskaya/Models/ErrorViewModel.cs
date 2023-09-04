7db3fb82b3312b246769bacd2b598dbadaf0a104
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-04 10:44:01 +0300

    Create ASP.Net Core Web App(MVC)

---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153501_Antilevskaya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
