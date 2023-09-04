f81755d874077d200b032a31218c20d8d3955ded
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-04 12:08:06 +0300

    Create new HomeController

---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/Controllers/Home.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153501_Antilevskaya.Controllers
+{
+    [Controller]
+    public class Home
+    {
+        public string Index()
+        {
+            return ("Hello World!");
+        }
+    }
+}
