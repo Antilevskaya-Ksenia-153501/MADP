f9fe8de5a3d92dc5bf91b108850c2e3480b1fb48
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-06 21:07:42 +0300

    Add model ListDemo & select with it in Index.cshtml

--- Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/Controllers/Home.cs
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/Controllers/Home.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WEB_153501_Antilevskaya.Models;
 
 namespace WEB_153501_Antilevskaya.Controllers
 {
     public class Home : Controller
     {
+        IEnumerable<ListDemo> listExample = new List<ListDemo>()
+        {
+            new ListDemo(1, "Item 1"),
+            new ListDemo(2, "Item 2"),
+            new ListDemo(3, "Item 3")
+        };
         public IActionResult Index()
         {
             ViewData["TitlePage"] = "Лабораторная работа 2";
+            ViewBag.ListExample = new SelectList(listExample, "Id", "Name");
             return View();
         }
     }
---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/Models/ListDemo.cs
@@ -0,0 +1,14 @@
+﻿namespace WEB_153501_Antilevskaya.Models
+{
+    public class ListDemo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ListDemo(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
