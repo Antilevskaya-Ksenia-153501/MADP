7f72a0075adb2203edf6b064a471d1eb032d2880
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-07 01:24:03 +0300

    Using view component for cart in userInfo

---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya/ViewComponents/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153501_Antilevskaya.ViewComponents
+{
+    public class Cart:ViewComponent
+    {
+        public string Invoke()
+        {
+            return "00,0 руб (0)";
+        }
+    }
+}
