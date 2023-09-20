b2cfc90446137733a05b75a4a46766be08828a94
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-20 12:05:29 +0300

    Change appereance

--- Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya.Domain/Entities/Category.cs
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya.Domain/Entities/Category.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WEB_153501_Antilevskaya.Domain.Entities
+﻿namespace WEB_153501_Antilevskaya.Domain.Entities;
+public class Category
 {
-    public class Category
-    {
-        int Id { get; set; }
-        public string Name { get; set; }
-        public string NormalizedName { get; set; }
-        public List<Exhibit> Exhibits { get; set; }
-    }
+    int Id { get; set; }
+    public string Name { get; set; }
+    public string NormalizedName { get; set; }
+    public List<Exhibit> Exhibits { get; set; }
 }
