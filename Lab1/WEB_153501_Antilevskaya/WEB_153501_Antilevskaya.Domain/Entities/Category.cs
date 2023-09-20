c31f7603d5de6a2efea54595f9512c91f8cd7ce2
Ksenia Antilevskaya <antikevkun@gmail.com>
2023-09-20 11:52:11 +0300

    Create entities Category & Exhibit in library of classes

---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_153501_Antilevskaya.Domain.Entities
+{
+    public class Category
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+        public List<Exhibit> Exhibits { get; set; }
+    }
+}
---
+++ Lab1/WEB_153501_Antilevskaya/WEB_153501_Antilevskaya.Domain/Entities/Exhibit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_153501_Antilevskaya.Domain.Entities;
+
+public class Exhibit
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Category? Category { get; set; }
+    public int Price { get; set; }
+    public string Image { get; set; }
+}
